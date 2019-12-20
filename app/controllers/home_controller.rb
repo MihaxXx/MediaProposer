c03013f0977ffe395a687f3a9117118f9e503cb6
Mikhail <mikes22@yandex.ru>
2019-12-20 15:18:56 +0300

    Add KP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @response = RestClient.get 'http://kinopoiskapiunofficial.tech/api/v2/films?keyword=Star', {Authorization: ENV['KP_TOKEN']}
+    JSON.parse(@response.body)
   end
+
 end
