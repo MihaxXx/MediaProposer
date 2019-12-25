c2112464fe340ea8401e19ad04960b1fe6485fcc
Mikhail <mikes22@yandex.ru>
2019-12-25 12:55:35 +0300

    Fix Unicode chars in search request and extract token one time

--- app/services/kinopoisk_api.rb
+++ app/services/kinopoisk_api.rb
@@ -1,12 +1,17 @@
+require 'addressable/uri'
+
 ## Class with methods to get films from unofficial Kinopoisk API
 class KinopoiskApi
   @base_url = 'http://kinopoiskapiunofficial.tech/'
-#TODO: Extract one time to class var
+  @token = ENV['KP_TOKEN']
+
   def self.find(name)
-    JSON.parse((RestClient.get @base_url + 'api/v2/films?keyword=' + name, {Authorization: ENV['KP_TOKEN']}).body)['films']
+    uri = Addressable::URI.escape((@base_url + 'api/v2/films?keyword=' + name))
+    JSON.parse((RestClient.get uri, Authorization: @token).body)['films']
   end
 
   def self.get(id)
-    JSON.parse((RestClient.get @base_url + 'api/v2/films/' + id, {Authorization: ENV['KP_TOKEN']}).body)
+    uri = Addressable::URI.escape((@base_url + 'api/v2/films/' + id))
+    JSON.parse((RestClient.get uri, Authorization: @token).body)
   end
 end
